e9ceddb7d1e6acbcb05c9b067f098b26ded12390
muudzo <tatendawalter62@gmail.com>
2025-01-13 06:15:03 +0100

    changes

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const display = document.getElementById("display")
 function appendToDisplay(input){
     display.value += input;
 }
+function newFunction(value) {
+    console.log('appending:', value);
+}
+
 function calculate(){
     display.value=eval(display.value)
+   
 }
 function clearDisplay(){
     display.value ="";
+   
 }
